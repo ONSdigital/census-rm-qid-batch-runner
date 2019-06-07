e63dfb9747996a695d84b5ebcdb516440d9d0ac4
liamtoozer <liam.toozer@gmail.com>
2019-06-07 09:44:58 +0100

    Remove unnecessary call

--- run_acceptance_tests.sh
+++ run_acceptance_tests.sh
@@ -9,5 +9,4 @@ BATCH_ID=${OUTPUT: -36}
 sleep 15
 
 mkdir print_files
-pipenv run python generate_print_files.py test_batch.csv print_files $BATCH_ID --no-gcs
 pipenv run python acceptance_test_check.py --batch-id $BATCH_ID
