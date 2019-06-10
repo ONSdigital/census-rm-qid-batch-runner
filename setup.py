db870900478d84b0066c10da6d65912eb4407d52
AdamHawtin <adam.hawtin@ons.gov.uk>
2019-06-10 07:42:07 +0100

    Remove author/email

--- setup.py
+++ setup.py
@@ -5,8 +5,6 @@ import setuptools
 setuptools.setup(
     name="census-rm-qid-batch-runner",
     version="0.0.1",
-    author="Adam Hawtin",
-    author_email="adam.hawtin@qa.com",
     description="Scripts to request qid pairs and generate print files",
     long_description=Path('README.md').read_text(),
     long_description_content_type="text/markdown",
