4abd58a6d5ae8d342c9921e4310ddc437f4897b7
AdamHawtin <adam.hawtin@ons.gov.uk>
2019-06-12 09:29:10 +0100

    Amend url

--- setup.py
+++ setup.py
@@ -8,6 +8,6 @@ setuptools.setup(
     description="Scripts to request qid pairs and generate print files",
     long_description=Path('README.md').read_text(),
     long_description_content_type="text/markdown",
-    url="https://github.com/ONSdigital/census-rm-qid-batch-runner/tree/setup-for-importing",
+    url="https://github.com/ONSdigital/census-rm-qid-batch-runner",
     packages=setuptools.find_packages(),
 )
