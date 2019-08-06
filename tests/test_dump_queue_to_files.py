963f26bd0e8ed92a1e671fb7036ed08e616e7f7c
Jamie Cox <me@jcox.tech>
2019-08-06 15:42:54 +0100

    Change to using a single dump file with multiple lines
    Added handling the restoration of old-style *.json files with --separate-files in dump_files_to_queue.py

--- tests/test_dump_queue_to_files.py
+++ tests/test_dump_queue_to_files.py
@@ -1,15 +1,21 @@
+import json
+import uuid
+from pathlib import Path
 from unittest.mock import Mock
 
 from dump_queue_to_files import _rabbit_message_received_callback
 
 
-def test_rabbit_message_consume(cleanup_test_files):
-    _rabbit_message_received_callback(Mock(), Mock(), None, b'{"key":"value"}', cleanup_test_files)
+def test_rabbit_message_consume(cleanup_test_files: Path):
+    output_file_path = cleanup_test_files.joinpath(f'{str(uuid.uuid4())}.dump')
 
-    index = 0
+    for i in range(5):  # simulate publishing 5 messages
+        message = f'{{"{i}_entry":"{i}_value"}}'.encode('utf-8')
+        _rabbit_message_received_callback(Mock(), Mock(), None, message, output_file_path)
 
-    for index, file_path in enumerate(cleanup_test_files.rglob('*.json')):
-        with open(file_path) as message_file:
-            assert '{"key":"value"}' == message_file.read()
+    for file_count, file_path in enumerate(cleanup_test_files.rglob('*.dump'), start=1):
+        with file_path.open() as message_file:
+            for index, message_line in enumerate(message_file):
+                assert json.loads(message_line)[f'{index}_entry'] == f'{index}_value'
 
-    assert index + 1 == 1
+    assert file_count == 1, "should only be a single *.dump file"
