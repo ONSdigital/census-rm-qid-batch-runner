24a9bfcb82aec67353a5b4ee6eeaa56bc00663b7
LeoKHoward <leo-howard@hotmail.co.uk>
2019-11-05 14:32:37 +0000

    Removed uneeded headers variable

--- rabbit_context.py
+++ rabbit_context.py
@@ -51,8 +51,7 @@ class RabbitContext:
             exchange=self._exchange,
             routing_key=self.queue_name,
             body=message,
-            properties=pika.BasicProperties(content_type=content_type, headers=headers,
-                                            delivery_mode=PERSISTENT_DELIVERY_MODE))
+            properties=pika.BasicProperties(content_type=content_type, delivery_mode=PERSISTENT_DELIVERY_MODE))
 
 
 class RabbitConnectionClosedError(Exception):
