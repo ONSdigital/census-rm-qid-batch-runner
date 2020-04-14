5ee043dd42c18000a533e40c4bd041dde0218ae9
Ryan Grundy <ryan.grundy@ons.gov.uk>
2020-04-14 14:05:29 +0100

    Added AT

--- acceptance_tests/sftp_print_files.feature
+++ acceptance_tests/sftp_print_files.feature
@@ -1,8 +1,13 @@
 Feature: Upload print files to a remote SFTP
   The batch runner can upload print files to a remote SFTP using SSH and a private key
 
-  Scenario: Upload generated print files to SFTP
-    Given the print files have been previously generated
+  Scenario Outline: Upload generated print files to SFTP
+    Given the print files have been previously generated from the "<sample_file>"
     And the correct SFTP config has been provided
-    When the SFTP utility is used to copy the print files
-    Then the print files are present on the remote SFTP
+    When the SFTP utility is used to copy the print files to the "<supplier>" directory
+    Then the print files are present on the remote "<supplier>" SFTP
+
+    Examples:
+      | sample_file       | expected_uacs | manifest_count | supplier |
+      | D_CCS_CH1_test    | 40            | 4              | QM       |
+      | D_ICCE_ICL2B_test | 40            | 4              | PPO       |
--- acceptance_tests/steps/sftp_print_files.py
+++ acceptance_tests/steps/sftp_print_files.py
@@ -3,32 +3,34 @@ from pathlib import Path
 
 from behave import given, when, then, step
 
+from mappings import SUPPLIER_TO_SFTP_DIRECTORY
 from sftp import SftpUtility
 from generate_print_files import copy_files_to_sftp
 
 
-@given('the print files have been previously generated')
-def print_files_exist(context):
-    context.file_paths = list(Path(__file__).parents[1].resolve().joinpath('resources').glob('D_CCS_CH1_test.*'))
+@given('the print files have been previously generated from the "{sample_file}"')
+def print_files_exist(context, sample_file):
+    context.file_paths = list(Path(__file__).parents[1].resolve().joinpath('resources').glob(f'{sample_file}.*'))
     for fp in context.file_paths:
         assert fp.is_file()
 
 
 @step('the correct SFTP config has been provided')
 def sftp_config_exists(context):
-    required_env_vars = ('SFTP_QM_DIRECTORY', 'SFTP_HOST', 'SFTP_USERNAME', 'SFTP_KEY_FILENAME', 'SFTP_PASSPHRASE')
+    required_env_vars = (
+        'SFTP_QM_DIRECTORY', 'SFTP_PPO_DIRECTORY', 'SFTP_HOST', 'SFTP_USERNAME', 'SFTP_KEY_FILENAME', 'SFTP_PASSPHRASE')
     for env_var in required_env_vars:
         assert os.environ[env_var] is not None
 
 
-@when('the SFTP utility is used to copy the print files')
-def upload_print_files_to_sftp(context):
-    copy_files_to_sftp(context.file_paths, 'QM')
+@when('the SFTP utility is used to copy the print files to the "{directory}" directory')
+def upload_print_files_to_sftp(context, directory):
+    copy_files_to_sftp(context.file_paths, directory)
 
 
-@then('the print files are present on the remote SFTP')
-def check_print_files_are_in_sftp(context):
-    with SftpUtility(os.getenv('SFTP_QM_DIRECTORY')) as sftp_client:
+@then('the print files are present on the remote "{supplier}" SFTP')
+def check_print_files_are_in_sftp(context, supplier):
+    with SftpUtility(SUPPLIER_TO_SFTP_DIRECTORY[supplier]) as sftp_client:
         sftp_contents = sftp_client._sftp_client.listdir('.')
         assert all(fp.name in sftp_contents for fp in context.file_paths)
         for fp in context.file_paths:
