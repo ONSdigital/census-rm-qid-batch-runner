211cb744c7c5dd8d18ada2fdc3954dac614a4bf0
LeoKHoward <leo-howard@hotmail.co.uk>
2020-05-18 12:07:52 +0100

    Updated mappings as per confluence

--- constants.py
+++ constants.py
@@ -4,6 +4,11 @@ from enum import Enum
 class Dataset(Enum):
     QM3_1 = 'QM3.1'
     PPD1_1 = 'PPD1.1'
+    PPD1_2 = 'PPD1.2'
+    PPD1_3 = 'PPD1.3'
+    PPD1_6 = 'PPD1.6'
+    PPD1_7 = 'PPD1.7'
+    PPD1_8 = 'PPD1.8'
 
 
 class PrintTemplate:
